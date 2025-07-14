c1e93bc5cf908bc1a542924a1ff583e918c13892
Emiliano Alvarado <natahelemiliano292@gmail.com>
2025-07-14 12:21:52 -0600

    modulo5.1

---
+++ app.py
@@ -0,0 +1,25 @@
+# main.py
+
+from modulo1 import capturar_datos_usuario
+from ejerciciomodulo2 import obtener_ejercicios
+from modulo3 import generar_rutina
+from modulo4 import consejos_salud
+from modulo5 import main
+
+def main():
+    datos = capturar_datos_usuario()
+    ejercicios = obtener_ejercicios(datos["objetivo"])
+    rutina = generar_rutina(datos, ejercicios)
+    consejos = consejos_salud()
+
+    print(f"\n--- Rutina diaria para {datos['nombre']} ---")
+    for r in rutina:
+        print(f"- {r}")
+
+    print("\n--- Consejos de salud ---")
+    for c in consejos:
+        print(f"- {c}")
+
+if __name__ == "__main__":
+    main()
+
--- modulo5.py
+++ modulo5.py
@@ -1,3 +1,8 @@
+from modulo1 import capturar_datos_usuario
+from ejerciciomodulo2 import obtener_ejercicios
+from modulo3 import generar_rutina
+from modulo4 import consejos_salud
+
 def main():
     usuario = capturar_datos_usuario()
     ejercicios = obtener_ejercicios(usuario["objetivo"])
