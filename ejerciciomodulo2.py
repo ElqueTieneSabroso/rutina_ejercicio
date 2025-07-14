b58009a7be73362eadfde087aaa1cda79051d30e
IRowen-droid <rivera.salazar.erik1@gmail.com>
2025-07-14 11:49:24 -0600

    Update ejerciciomodulo2.py

--- ejerciciomodulo2.py
+++ ejerciciomodulo2.py
@@ -1,7 +1,8 @@
+#definimos las respuestas dependiendo de la eleccion de la persona
 def obtener_ejercicios(objetivo):
     ejercicios = {
-        "perder peso": ["Cardio", "Caminata rápida", "Burpees", "Jumping Jacks"],
-        "ganar músculo": ["Sentadillas con peso", "Flexiones", "Dominadas", "Peso muerto"],
-        "mantenerse": ["Yoga", "Bicicleta", "Natación", "Caminata"]
+        "perder peso": ["Cardio", "Caminata rápida", "Burpees", "Jumping Jacks"], #elecciones basadas en perder grasa eda
+        "ganar músculo": ["Sentadillas con peso", "Flexiones", "Dominadas", "Peso muerto"], #ponerse mamei
+        "mantenerse": ["Yoga", "Bicicleta", "Natación", "Caminata"] #cosas mas tranquilas que no terminen con tu vida
     }
-    return ejercicios.get(objetivo, ["Ejercicio general"])
+    return ejercicios.get(objetivo, ["Ejercicio general"]) #returneamos un ejercicio general pues, general 
